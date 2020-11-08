a0752a1fea77bad2a8290a72d89050b1ce60a06e
zfortes <jose.fortes.neto@gmail.com>
2020-11-07 22:25:37 -0300

    Modularizando o codigo

--- code.py
+++ code.py
@@ -1,70 +1,68 @@
-import numpy as np
 import pandas as pd
-import random
-from sklearn.model_selection import KFold
-import lightgbm as lgb
 from sklearn.metrics import classification_report
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import preprocessing
-
-df = pd.read_excel('dataset.xlsx')
-
-df['SARS-Cov-2 exam result'] = [0 if a == 'negative' else 1 for a in df['SARS-Cov-2 exam result'].values]
-
-Y = df['SARS-Cov-2 exam result']
-
-df = df.drop([
-    "SARS-Cov-2 exam result",
-    "Patient ID",
-    'Patient addmited to regular ward (1=yes, 0=no)',
-    'Patient addmited to semi-intensive unit (1=yes, 0=no)',
-    'Patient addmited to intensive care unit (1=yes, 0=no)'
-], axis=1)
-
-df = df.fillna(df.mean())
-
-# Fill NaNs with -10
-df = df.fillna(-10, axis=1)
-
-categorical_features = [key for key in dict(df.dtypes) if dict(df.dtypes)[key] in ['object'] ]
-
-X = pd.get_dummies(df, prefix=categorical_features, columns=categorical_features)
-
-clf = RandomForestClassifier(max_depth=50, random_state=0, n_estimators=40)
-# K = 10
-# folds = KFold(K, shuffle=True, random_state=40)
-
+from matplotlib import pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 
-    # X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-    # y_train, y_test = Y.iloc[train_index], Y.iloc[test_index]
+# pd.set_option('display.expand_frame_repr', False)
+# X.head(1)
+# print(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.40, random_state=55)
+def ler_arquivo():
+    return pd.read_excel('dataset.xlsx')
 
 
-clf.fit(X_train, y_train)
+def preparar_dataset(df):
+    df['SARS-Cov-2 exam result'] = [0 if a == 'negative' else 1 for a in df['SARS-Cov-2 exam result'].values]
 
-pred_y = clf.predict(X_test)
+    Y = df['SARS-Cov-2 exam result']
 
-print(classification_report(y_test, pred_y))
-pred = clf.predict(X_test)
-accuracy = accuracy_score(y_test, pred)
-print(f'Mean accuracy score: {accuracy:.3}')
+    df = df.drop([
+        "SARS-Cov-2 exam result",
+        "Patient ID",
+        'Patient addmited to regular ward (1=yes, 0=no)',
+        'Patient addmited to semi-intensive unit (1=yes, 0=no)',
+        'Patient addmited to intensive care unit (1=yes, 0=no)',
+        'Patient age quantile'
+    ], axis=1)
+    df = df.fillna(-10, axis=1)
+    columns = [key for key in dict(df.dtypes) if dict(df.dtypes)[key] in ['object']]
+    X = pd.get_dummies(df, prefix=columns, columns=columns)
+    return X, Y
 
-#
-# for fold, (train_index, test_index) in enumerate(folds.split(X, Y)):
-#     print('Fold:', fold + 1)
-#     X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-#     y_train, y_test = Y.iloc[train_index], Y.iloc[test_index]
-#
-#     clf.fit(X_train, y_train)
-#
-#     pred_y = clf.predict(X_test)
-#
-#     print(classification_report(y_test, pred_y))
-#     pred = clf.predict(X_test)
-#     accuracy = accuracy_score(y_test, pred)
-#     print(f'Mean accuracy score: {accuracy:.3}')
-#
+
+def treina_classificador_random_forest(x_train, y_train):
+    clf = RandomForestClassifier(max_depth=50, random_state=0, n_estimators=40)
+    clf.fit(x_train, y_train)
+    return clf
+
+
+def verifica_categorias_importantes(clf, x_train):
+    columns = pd.DataFrame(clf.feature_importances_, index=x_train.columns,
+                                       columns=['importance']).sort_values('importance', ascending=False)
+    pf = columns.head(10)
+
+    feat_importances = pd.Series(clf.feature_importances_, index=x_train.columns)
+    # feat_importances.nlargest(10).plot(kind='barh')
+    # plt.title("Top 10 important features")
+    # plt.show()
+    print(pf)
+
+
+def run():
+    df = ler_arquivo()
+    x, y = preparar_dataset(df)
+    print(x)
+    print(len(x))
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.40, random_state=55)
+    clf = treina_classificador_random_forest(x_train, y_train)
+    pred_y = clf.predict(x_test)
+    print(pred_y)
+    pred = clf.predict(x_test)
+    accuracy = accuracy_score(y_test, pred)
+    print(f'Mean accuracy score: {accuracy:.3}')
+    clf.score(x_test, y_test)
+    print(classification_report(y_test, pred_y))
+    verifica_categorias_importantes(clf, x_train)
---
+++ main.py
@@ -0,0 +1,4 @@
+from code import run
+
+run()
+
